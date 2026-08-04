75877f1d97ada2a5cf47916413469e50d8359292
Aaron Hardy <aaronhardy6@gmail.com>
2026-08-04 15:03:32 -0400

    update mobile styles

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -142,7 +142,7 @@ const Project = () => {
 
         .pj-thumb-frame {
           position: relative;
-          width: 400px;
+          max-width: 400px;
           padding: 8px;
           border: 1px solid var(--frame);
           flex-shrink: 0;
@@ -211,7 +211,7 @@ const Project = () => {
                     <img
                       src={proj.img_src}
                       alt={proj.alt}
-                      className="w-full max-w-[400px]"
+                      className="w-full max-w-[300px] md:max-w-[400px]"
                     />
                   </div>
                 </motion.div>
