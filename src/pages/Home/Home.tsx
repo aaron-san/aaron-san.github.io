0ca9b9a65d516050ccd7b5a29813681ec0072161
Aaron Hardy <aaronhardy6@gmail.com>
2026-08-04 08:54:51 -0400

    update homepage profile

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -21,56 +21,57 @@ const Home = (): JSX.Element => {
 
           <div className="mx-auto my-4 max-w-[400px] text-slate-600 text-center">
             <div className="sm:block inline mx-auto w-fit">
-              Hello! I'm Aaron Hardy, <span className="italic">CFA, CAIA</span>,{" "}
+              Hello! I'm Aaron Hardy, <span className="">CFA, CAIA</span>
+              ,{" "}
             </div>
-            <div className="mx-auto mt-2 border-stone-300 border-b w-fit text-lg italic">
+            <div className="mx-auto mt-2 border-stone-300 border-b w-fit text-sm pb-4">
               {" "}
-              <b>financial analyst, data analyst, </b> and{" "}
-              <b>investment professional</b>
+              <b>financial analyst – data analyst – investment professional</b>
             </div>
-            <div className="sm:block inline mt-4 w-fit">
-              <div className="inline mr-1">I specialize in</div>
-              <a
-                href="https://aaron-san.github.io/cfa-notes/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="inline text-600 italic"
-              >
-                financial analysis
-              </a>
-              <span className="pr-1">,</span>
-              <span className="italic">accounting</span> and
-              <a
-                href="https://react.dev"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="inline ml-1 w-fit bold italic"
-              >
-                React
-              </a>
-              .
+            <ul className="mt-4 text-left tracking-wide text-sm">
+              <li className="relative pl-6">
+                <span className="absolute left-0 top-0 text-green-600">✓</span>
+                CFA and CAIA charter holder with 4+ years of experience in
+                financial modeling and risk analysis
+              </li>
+
+              <li className="relative pl-6 mt-2">
+                <span className="absolute left-0 top-0 text-green-600">✓</span>
+                Proven ability to lead analyst team, analyze private equity and
+                loan deals, build pro forma models, and translate complex
+                financial data into actionable insights for executive
+                leadership.
+              </li>
+
+              <li className="relative pl-6 mt-2">
+                <span className="absolute left-0 top-0 text-green-600">✓</span>
+                Seeking an analyst role where analytical rigor and commercial
+                judgment drive outcomes.
+              </li>
+            </ul>
+
+            <div className="sm:block inline mt-4 pl-2 mx-auto">
               <div className="sm:block inline mx-auto w-fit">
-                <span>I have experience in finance and have passed all</span>{" "}
+                <div>I am a member of the</div>
+                <div>
                 <a
                   href="https://www.cfainstitute.org"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  {" "}
-                  CFA® exams
+                  CFA<sup className="text-sm">®</sup>
                 </a>{" "}
-                and{" "}
-                <a
+                Institute and {" "}<a
                   href="https://caia.org"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  CAIA exams
-                </a>
-                .
+                  CAIA
+                </a> association.
+                </div>
               </div>
-              <div className="sm:block inline mx-auto w-fit">
-                Contact me on{" "}
+              <div className="sm:block inline mx-auto w-fit text-sm mt-4">
+                See me on{" "}
                 <a
                   href="https://www.linkedin.com/in/aaron-hardy-651b2410/"
                   target="_blank"
