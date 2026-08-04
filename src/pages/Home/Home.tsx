cbec412122594c2cc2074d3c0d7d9c5e60d9604b
Aaron Hardy <aaronhardy6@gmail.com>
2026-08-04 08:24:42 -0400

    update homepage

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,11 @@
 import React, { useState } from "react";
 import type { JSX } from "react";
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
 
 import Projects from "../../components/Projects";
 import OtherProjects from "../../components/OtherProjects";
 
-
-const Home = (): JSX.Element =>
-{
+const Home = (): JSX.Element => {
   return (
     <section className="mx-auto max-w-[90%] sm:max-w-[95%]">
       <div className="mt-12">
@@ -17,29 +15,31 @@ const Home = (): JSX.Element =>
             alt="Profile picture of Aaron Hardy"
             className="mx-auto w-[260px]"
             initial={{ opacity: 0, y: 20 }} // Start invisible and slightly below
-            animate={{ opacity: 1, y: 0 }}  // Animate to visible and in place
-            transition={{ duration: 0.6 }}  // Duration of animation in seconds
+            animate={{ opacity: 1, y: 0 }} // Animate to visible and in place
+            transition={{ duration: 0.6 }} // Duration of animation in seconds
           />
 
           <div className="mx-auto my-4 max-w-[400px] text-slate-600 text-center">
             <div className="sm:block inline mx-auto w-fit">
-              Hello! I'm Aaron Hardy, <span className="italic">CFA</span>, </div>
-            <div className="mx-auto mt-2 border-stone-300 border-b w-fit text-lg italic"> <b>financial analyst</b> and <b>web developer</b>
+              Hello! I'm Aaron Hardy, <span className="italic">CFA</span>,{" "}
             </div>
-            <div
-
-              className="sm:block inline mt-4 w-fit">
-              <div className="inline mr-1">
-                I specialize in
-              </div>
-
-
+            <div className="mx-auto mt-2 border-stone-300 border-b w-fit text-lg italic">
+              {" "}
+              <b>financial analyst, data analyst, </b> and{" "}
+              <b>investment professional</b>
+            </div>
+            <div className="sm:block inline mt-4 w-fit">
+              <div className="inline mr-1">I specialize in</div>
               <a
                 href="https://aaron-san.github.io/cfa-notes/"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="inline text-600 italic">financial analysis</a><span className="pr-1">,</span>
-                <span className="italic">accounting</span> and
+                className="inline text-600 italic"
+              >
+                financial analysis
+              </a>
+              <span className="pr-1">,</span>
+              <span className="italic">accounting</span> and
               <a
                 href="https://react.dev"
                 target="_blank"
@@ -47,24 +47,37 @@ const Home = (): JSX.Element =>
                 className="inline ml-1 w-fit bold italic"
               >
                 React
-              </a>.
-
+              </a>
+              .
               <div className="sm:block inline mx-auto w-fit">
-                <span>
-                  I have experience in finance and have passed all</span> <a
-                    href="https://www.cfainstitute.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-
-                  > CFA® exams</a> and <a href="https://caia.org"
-
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >CAIA exams
-                </a>.
+                <span>I have experience in finance and have passed all</span>{" "}
+                <a
+                  href="https://www.cfainstitute.org"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {" "}
+                  CFA® exams
+                </a>{" "}
+                and{" "}
+                <a
+                  href="https://caia.org"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  CAIA exams
+                </a>
+                .
               </div>
               <div className="sm:block inline mx-auto w-fit">
-                Contact me on <a href="https://www.linkedin.com/in/aaron-hardy-651b2410/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+                Contact me on{" "}
+                <a
+                  href="https://www.linkedin.com/in/aaron-hardy-651b2410/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  LinkedIn
+                </a>
               </div>
             </div>
           </div>
@@ -82,7 +95,6 @@ const Home = (): JSX.Element =>
         </div>
         <OtherProjects />
       </div>
-      
     </section>
   );
 };
